ae502fbc170ab49bff8ff6a461ce6a7755bb1606
narges-rezaei <narges-rezaei@hotmail.com>
2023-03-10 12:01:48 +0100

    commit

--- back-end/controller/profile.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express();
-const profile = require('../services/profile');
-
-
-app.get("/init-user-info", (req, res) => {
-  
-});
-module.exports = app;
