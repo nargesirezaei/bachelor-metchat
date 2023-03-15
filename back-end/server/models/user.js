dbfd15458749a65a0ee3fa8387d41c3340967439
Amitaf1 <abbasaliz28@icloud.com>
2023-03-15 02:00:57 +0100

    added vars for reg + ability to add bio and avatar

--- back-end/server/models/user.js
+++ back-end/server/models/user.js
@@ -1,13 +1,54 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
 const userSchema = new Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  password: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  firstName : {
+      type : String,
+      required : true,
+      min : 3,
+  },
+  lastName : {
+      type : String,
+      required : true,
+      min : 3,
+  },
+  email : {
+      type : String,
+      required : true,
+      unique : true,
+      max : 50,
+  },
+  password : {
+      type : String,
+      required : true,
+      min : 8,
+  },
+
+  /*contacts : {
+      type : Array,
+      required : true,
+      default : [],
+  },*/
+
+  isBioSet : {
+      type : Boolean,
+      default : false,
+  },
+  avatarImage : {
+      type : String,
+      default : "",
+  },
+  bio : {
+      type : String,
+      default : "",
+  },
+  /*interests : {
+      type : String,
+      default : "",
+  },*/
   isAdmin:{type:Boolean,default:false}
-});
+}); 
 
 const User = mongoose.model('User', userSchema);
 
