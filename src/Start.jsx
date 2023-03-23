a7ee1423b8804e23fedfc7a839601091348e0083
Aina <s338849@oslomet.no>
2023-03-23 15:01:38 +0100

    minor changes

--- src/Start.jsx
+++ src/Start.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
 import { startRoute } from "./TempRoutes";
 
 function Start() {}
+
+export default Start;
