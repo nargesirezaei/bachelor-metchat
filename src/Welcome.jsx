bb7c4244659244048096bf54cfa37fa17237b665
Aina <s338849@oslomet.no>
2023-03-28 12:21:28 +0200

    Made Register.jsx - to test intergration with backend.

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
-import { welcomeRoute } from "./APIRoutes";
+//import { useState, useEffect } from "react";
+//import { Link, useNavigate } from "react-router-dom";
+//import axios from "axios";
+//import { welcomeRoute } from "./APIRoutes";
 import Login from "./components/Login";
 import "./home.css";
 
---
+++ src/components/Register.jsx
@@ -0,0 +1,129 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { authenticationRoute } from "../APIRoutes";
+
+function Register() {
+  const [values, setValues] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+
+  const handleValidation = () => {
+    const { fname, lname, email, password, confirmPassword } = values;
+    if (password !== confirmPassword) {
+      alert("Feltene for passord er ikke like.");
+      return false;
+    } else if (fname.length < 3) {
+      alert("Fornavn må være minst tre bokstaver.");
+      return false;
+    } else if (lname.length < 2) {
+      alert("Etternavn må være minst to bokstaver.");
+      return false;
+    } else if (
+      !/^[a-zæøåA-ZÆØÅ()]+$/.test(fname) ||
+      !/^[a-zæøåA-ZÆØÅ()]+$/.test(lname)
+    ) {
+      alert(
+        "Navnet ditt kan bare bestå av bokstaver fra det norske alfabetet."
+      );
+      return false;
+    } else if (password.length <= 8) {
+      alert("Passordet må være minst åtte(8) tegn.");
+      return false;
+    } else if (
+      !/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+        email
+      )
+    ) {
+      alert("E-post er i feil format.");
+      return false;
+    } // regex for e-post
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (handleValidation()) {
+      const { fname, lname, email, password } = values;
+      const { data } = await axios.post(`${authenticationRoute}/register`, {
+        firstName: fname,
+        lastName: lname,
+        email,
+        password,
+      });
+
+      if (data.status === 500 || data.status === false) {
+        alert(data.msg);
+      } else if (data.status === true) {
+        localStorage.setItem("metchat-user", JSON.stringify(data.user));
+        alert("Bruker opprettet"); // Noe bedre å si?
+      }
+    }
+  };
+
+  return (
+    <div className="content" id="reg">
+      <form id="form_reg" onSubmit={(e) => handleSubmit(e)}>
+        <div className="form-row">
+          <label>Fornavn</label>
+          <input
+            type="text"
+            name="fname"
+            required
+            id="first_name"
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div className="form-row">
+          <label>Etternavn</label>
+          <input
+            type="text"
+            name="lname"
+            required
+            id="last_name"
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div className="form-row">
+          <label>Epost</label>
+          <input
+            type="text"
+            name="email"
+            required
+            id="email_reg"
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div className="form-row">
+          <label>Passord</label>
+          <input
+            type="password"
+            name="password"
+            required
+            id="password_reg"
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div className="form-row">
+          <label>Gjente Passord</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            required
+            id="repeat_passowrd"
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+      </form>
+      <button className="btn-reg">Register</button>
+    </div>
+  );
+}
+
+export default Register;
