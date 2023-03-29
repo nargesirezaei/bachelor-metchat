92661792c8f5a82bdc97b516179afc3b4831db58
Aina <s338849@oslomet.no>
2023-03-29 20:21:22 +0200

    Played with routes. Added content to MoreInfo.jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 // lager lenke til test register-filen for å teste tilkobling mellom backend og frontend
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Register from "./test_pages/Register";
+import Login from "./components/Login";
+//import Register from "./test_pages/Register";
+import Register from "./components/Register";
+import MoreInfo from "./MoreInfo";
 import Welcome from "./Welcome";
 
 export default function App() {
@@ -9,7 +12,11 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Welcome />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/moreInfo" element={<MoreInfo />} />
+
+        {/*<Route path="/register" element={<Register />} />*/}
       </Routes>
     </BrowserRouter>
   );
