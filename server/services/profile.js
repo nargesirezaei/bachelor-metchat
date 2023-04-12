f9b2b9a59fa57d1aa086fbdb0cbee3aad8af9e81
Amitaf1 <abbasaliz28@icloud.com>
2023-04-12 11:44:00 +0200

    profile.js - as it was before (commented out)

--- server/services/profile.js
+++ server/services/profile.js
@@ -57,3 +57,40 @@ module.exports = {
         };
     },
 };
+/**
+ * const Profile = require("../models/profile");
+
+module.exports = {
+    create: async (req, res) => {
+        const { userId, avatarImage, bio } = req.body;
+        
+        await Profile.create( {
+            userId,
+            avatarImage,
+            bio,
+        })
+        .then(() => {
+            return res.status(200).json({ message: "Successfuly created bio" });
+        })
+        .catch((error) => {
+            return res.status(401).json({ message: Unsuccessfuly created bio: ${error} });
+        });
+    },
+
+    edit: async (req, res) => {
+        const { userId, avatarImage, bio } = req.body;
+        
+        await Profile.findOneAndUpdate( { userId }, {
+            avatarImage,
+            bio,
+        })
+        .then(() => {
+            return res.status(200).json({ message: "Successfuly edited bio" });
+        })
+        .catch((error) => {
+            return res.status(401).json({ message: Unsuccessfuly edited bio: ${error} });
+        });
+    },
+};
+
+ */
