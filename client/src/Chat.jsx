c102f19e274de8309f515414a7b52f41fa4d8a45
Aina <s338849@oslomet.no>
2023-04-17 11:43:44 +0200

    Strange changes were made to Welcome.jsx. Restored to previous functionality. Changed the commented out section in home.css to match. Added import of socket.io in Chat.jsx

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { io } from "socket.io-client";
 import { /*Link,*/ useNavigate } from "react-router-dom";
 import { /*profileRoute,*/ conversationRoute } from "./APIRoutes";
 import Nav from "./components/MainNav";
@@ -81,22 +82,10 @@ function Chat() {
 
         {/* RIGHT */}
         <div className="col-md-3" id="right-side">
-          <img class="part-1" src="face.png" alt="face-icon" />
+          <img class="part-1" src="profile.svg" alt="profile-icon" />
           <div class="part-2">
             <img src="profile.svg" alt="profile-icon" />
-            <div class="search-icon">
-              <i class="bi bi-search"></i>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="35"
-                height="35"
-                fill="currentColor"
-                class="bi bi-search"
-                viewBox="0 0 16 16"
-              >
-                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-              </svg>
-            </div>
+            <p>Profil</p>
           </div>
           <div class="part-3">
             <p>Profil</p>
