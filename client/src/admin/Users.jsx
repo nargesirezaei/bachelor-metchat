5b1c1eac5fe724f02d90bcd19250d0c8cf67e2a3
Aina <s338849@oslomet.no>
2023-04-25 16:05:46 +0200

    Adjusted bootstrap col. - so that the collums shows next to each other. Styling continues. - Still trying to figure out: -- how to make the table on the right side take up more room. -- placement of the buttons on the bottom right.

--- client/src/admin/Users.jsx
+++ client/src/admin/Users.jsx
@@ -1,4 +1,5 @@
 import AdminNav from "./AdminNav";
+import dummyProfile from "../img/profile.svg";
 
 function Users() {
   return (
@@ -6,7 +7,7 @@ function Users() {
       <AdminNav />
 
       <div className="row admin-users">
-        <div className="col-sm-4 admin-users-list">
+        <div className="col-sm-6">
           <h1>Brukere</h1>
 
           {/* -- Search bar --
@@ -27,44 +28,60 @@ function Users() {
           {/* -- List of users -- */}
           <ul className="user-list">
             <li className="user-item">
-              <img src="./img/profile.svg" alt="P" />
+              <img src={dummyProfile} alt="P" />
               <span>Anna Lund</span>
+            </li>
+            <li className="user-item">
+              <img src={dummyProfile} alt="P" />
+              <span>Anna Mund</span>
+            </li>
+            <li className="user-item">
+              <img src={dummyProfile} alt="P" />
+              <span>Anna Nund</span>
+            </li>
+            <li className="user-item">
+              <img src={dummyProfile} alt="P" />
+              <span>Anna Ound</span>
             </li>
           </ul>
         </div>
-        <div classNameName="col-sm-8">
-          <aside>
-            {/* -- User profile -- */}
-            <div className="user">
-              <div className="user-header">
-                <img src="./img/profile.svg" alt="P" />
-                <span>Fornavn </span>
-                <span>Etternavn</span>
-              </div>
-              <table className="user-info">
+
+        <div className="col-sm-5 profile-right">
+          {/* -- User profile -- */}
+          <div className="user">
+            <div className="user-header">
+              <img src={dummyProfile} alt="P" />
+              <span>Fornavn </span>
+              <span>Etternavn</span>
+            </div>
+
+            <table className="user-info">
+              <tbody>
                 <tr>
-                  <td>E-post:</td>
+                  <td className="mb-1">E-post:</td>
                   <td className="user-info-value">e-post</td>
                 </tr>
                 <tr>
-                  <td>Passord:</td>
+                  <td className="mb-1">Passord:</td>
                   <td className="user-info-value">passord</td>
                 </tr>
                 <tr>
-                  <td>Interesser:</td>
-                  <td className="user-info-value">interesse</td>
+                  <td className="mb-1">Interesser:</td>
+                  <td className="user-info-value">
+                    <p>interesse</p>
+                  </td>
                 </tr>
-              </table>
-            </div>
+              </tbody>
+            </table>
+          </div>
 
-            {/* -- Buttons for conversations -- */}
-            <div className="btn-coanversations">
-              <button>Se samtaler</button>
-              <button>Last ned samtaler</button>
-            </div>
-            {/* -- Button for deleting user -- */}
-            <button className="del-user">Slett bruker</button>
-          </aside>
+          {/* -- Buttons for conversations -- */}
+          <div className="btn-coanversations">
+            <button>Se samtaler</button>
+            <button>Last ned samtaler</button>
+          </div>
+          {/* -- Button for deleting user -- */}
+          <button className="del-user">Slett bruker</button>
         </div>
       </div>
     </>
