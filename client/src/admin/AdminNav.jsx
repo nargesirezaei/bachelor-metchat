89d6e68d4b7a60688669b71836b744373330ecd6
Aina <s338849@oslomet.no>
2023-04-25 10:23:36 +0200

    Adjusted the AdminNav to match the MainNav

--- client/src/admin/AdminNav.jsx
+++ client/src/admin/AdminNav.jsx
@@ -1,11 +1,55 @@
 import { Link } from "react-router-dom";
 import logo from "../img/Logo.svg";
+import { HiBars3 } from "react-icons/hi2";
+import { useState } from "react";
 
 function AdminNav() {
+  /* Toggle Nav*/
+  const [showLinks, setShowLinks] = useState(false);
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
   return (
-    <nav className="main-menu">
+    <nav className="nav-center">
+      <div className="nav-header">
+        <Link to="/" className="logo">
+          <img src={logo} alt="Logo" />
+        </Link>
+        <div className="nav-items">
+          <Link className="item" to="/admin/brukere">
+            Brukere
+          </Link>
+          <Link className="item" to="/admin/samtaler">
+            Samtaler
+          </Link>
+          <Link className="item" to="/logut">
+            Logg ut
+          </Link>
+        </div>
+
+        <HiBars3 className="nav-toggle" onClick={toggleLinks} />
+      </div>
+      <div
+        className={
+          showLinks ? "links-container show-container" : "links-container"
+        }
+      >
+        <Link className="item" to="/admin/brukere">
+          Brukere
+        </Link>
+        <Link className="item" to="/admin/samtaler">
+          Samtaler
+        </Link>
+        <Link className="item" to="/logut">
+          Logg ut
+        </Link>
+      </div>
+    </nav>
+  );
+}
+/* <nav className="nav-center">
       <Link to="/admin">
-        <img src={logo} alt="Logo" />
+       
       </Link>
       <ul className="main-nav">
         <li className="nav-item">
@@ -20,8 +64,6 @@ function AdminNav() {
           <Link to="/logut">Logg ut</Link>
         </li>
       </ul>
-    </nav>
-  );
-}
+      </nav>*/
 
 export default AdminNav;
