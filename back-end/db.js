1db3df9202b2693f9b1425e0b44205dff88c1ab2
narges-rezaei <narges-rezaei@hotmail.com>
2023-04-26 09:53:07 +0200

    --

--- back-end/db.js
+++ back-end/db.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 module.exports = {
-  connectToDb: (cb) => {
-    mongoose
-      .connect("mongodb://localhost:27017/chat", {
-        seNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .then(() => cb("Connected to MongoDB"))
-      .catch((err) => cb(err));
-  },
+    connectToDb: (cb) => {
+        mongoose
+            .connect("mongodb://127.0.0.1:27017/chat", {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            })
+            .then(() => cb("Connected to MongoDB"))
+            .catch((err) => cb(err));
+    },
 };
