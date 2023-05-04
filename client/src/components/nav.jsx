479f9b3617e2a107ff386d7e6dda78ac195e8b8c
Ramimarroun <70324229+Ramimarroun@users.noreply.github.com>
2023-05-04 15:27:05 +0200

    Fix the nav in Welcome page

--- client/src/components/nav.jsx
+++ client/src/components/nav.jsx
@@ -1,16 +1,37 @@
 import { Link } from "react-router-dom";
-import { Flex } from "./Flex";
-import { Logo } from "./logo";
+import Logo from "../assets/img/Logo.svg";
+import { HiBars3 } from "react-icons/hi2";
+import { useState } from "react";
 
 export const Nav = () => {
-    return (
-        <nav>
-            <Flex content="space-between" align="center" className="w-100">
-                <Logo />
-                <Link to="/about" className="text-light pe-3">
-                    Om Oss
-                </Link>
-            </Flex>
-        </nav>
-    );
+  /* Toggle Nav*/
+  const [showLinks, setShowLinks] = useState(false);
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+  return (
+    <nav className="nav-center">
+      <div className="nav-header">
+        <Link to="/">
+          <img src={Logo} />
+        </Link>
+
+        <a className="push" href="">
+          Om Oss
+        </a>
+
+        <HiBars3 className="nav-toggle" onClick={toggleLinks} />
+      </div>
+
+      <ul
+        className={
+          showLinks ? "links-container show-container" : "links-container"
+        }
+      >
+        <li>
+          <a href="">Om Oss</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
--- client/src/main/interests.jsx
+++ client/src/main/interests.jsx
@@ -4,54 +4,54 @@ import { Logo } from "../components/logo";
 import profile from "../assets/img/profile.svg";
 import pluss from "../assets/img/pluss.svg";
 export const Interests = () => {
-    return (
-        <>
-            <nav>
-                <div className="container" style={{ zIndex: 9999 }}>
-                    <Flex content="space-between" align="center">
-                        <Logo />
-                        <Flex className="m-0" gap={3}>
-                            <li className="push">
-                                <Link to="/logout" className="text-light">
-                                    Log ut
-                                </Link>
-                            </li>
-                        </Flex>
-                    </Flex>
-                </div>
-            </nav>
-            <section id="first">
-                <h1 className="text-center">
-                    Snart ferdig. Fortell oss litt om deg selv
-                </h1>
-                <img src={profile} className="profile-icon" />
-                <div className="box">
-                    <label className="text">Om Meg</label>
-                    <textarea></textarea>
-                    <h2>Intresser</h2>
-                    <div className="btns-odd">
-                        <button className="intress-btn">Mat</button>
-                        <button className="intress-btn">Knust</button>
-                        <button className="intress-btn">Litratur</button>
-                    </div>
-                    <div className="btns-even">
-                        <button className="intress-btn">Idrett</button>
-                        <button className="intress-btn">Dyr</button>
-                        <button className="intress-btn">Språk</button>
-                        <button className="intress-btn">Musikk</button>
-                    </div>
-                    <div className="showMore">
-                        <a href="">
-                            <img src={pluss} alt="pluss-icon" />
-                        </a>
-                        <button>Vis flere</button>
-                    </div>
-                </div>
-            </section>
-            <Flex className="btns" style={{ backgroundColor: "transparent" }}>
-                <button>Tilbake</button>
-                <button>Gå Videre</button>
+  return (
+    <>
+      <nav>
+        <div className="container" style={{ zIndex: 9999 }}>
+          <Flex content="space-between" align="center">
+            <Logo />
+            <Flex className="m-0" gap={3}>
+              <li className="push">
+                <Link to="/logout" className="text-light">
+                  Log ut
+                </Link>
+              </li>
             </Flex>
-        </>
-    );
+          </Flex>
+        </div>
+      </nav>
+      <section id="first">
+        <h1 className="text-center">
+          Snart ferdig. Fortell oss litt om deg selv
+        </h1>
+        <img src={profile} className="profile-icon" />
+        <div className="box">
+          <label className="text">Om Meg</label>
+          <textarea></textarea>
+          <h2>Intresser</h2>
+          <div className="btns-odd">
+            <button className="intress-btn">Mat</button>
+            <button className="intress-btn">Knust</button>
+            <button className="intress-btn">Litratur</button>
+          </div>
+          <div className="btns-even">
+            <button className="intress-btn">Idrett</button>
+            <button className="intress-btn">Dyr</button>
+            <button className="intress-btn">Språk</button>
+            <button className="intress-btn">Musikk</button>
+          </div>
+          <div className="showMore">
+            <a href="">
+              <img src={pluss} alt="pluss-icon" />
+            </a>
+            <button>Vis flere</button>
+          </div>
+        </div>
+      </section>
+      <Flex className="btns" style={{ backgroundColor: "transparent" }}>
+        <button>Tilbake</button>
+        <button>Gå Videre</button>
+      </Flex>
+    </>
+  );
 };
