f333edb31aeabdb77a5d2f5df8143bd58a898bf9
Aina <s338849@oslomet.no>
2023-05-20 19:10:05 +0200

    didn't import contactsAdmin.js from services

--- server/controller/contacts.js
+++ server/controller/contacts.js
@@ -3,6 +3,7 @@ const { verifyaccess } = require("../services/authorization");
 const app = express();
 
 const contacts = require("../services/contacts");
+const adminContacts = require("../services/contactsAdmin");
 
 // => contact/add or contact/remove or contact/mycontacts or update
 
@@ -19,8 +20,8 @@ app.get("/all-contacts", verifyaccess, contacts.allContacts);
 app.get("/update", verifyaccess, contacts.UpdateMyContact);
 
 // Feilmedling: "Route.get() requires a callback function but got a [object Undefined]""
-//app.get("/getUser/:userId", /*verifyaccess,*/ contacts.getUser);
+app.get("/getUser/:userId", /*verifyaccess,*/ adminContacts.getUser);
 
-//app.get("/getAllUsers", /*verifyaccess,*/ contacts.getAllUsers);
+app.get("/getAllUsers", /*verifyaccess,*/ adminContacts.getAllUsers);
 
 module.exports = app;
